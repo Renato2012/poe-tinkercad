5f67def6a987d0f9c615aa7db63b911e647f88f3
Renato Araújo <Renato2012@users.noreply.github.com>
2026-09-01 00:29:51 -0300

    Add código-fonte8

---
+++ codigos/modulo1/m1_atividade3_questaoB.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+}
+
+void loop() {}
